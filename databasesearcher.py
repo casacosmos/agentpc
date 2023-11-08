3f41408a187ef07643fd9930750302b71741a9e1
casacosmos <132779702+casacosmos@users.noreply.github.com>
2023-11-08 02:51:04 -0400

    Update databasesearcher.py

--- databasesearcher.py
+++ databasesearcher.py
@@ -9,117 +9,89 @@ from ast import literal_eval
 import openai
 
 
-# Define constants for the application
+# Initialization of OpenAI API key should ideally be handled more securely or outside of the code.
 
-EMBEDDING_MODEL = "text-embedding-ada-002"
-
-MAX_TOKENS = 8000
+openai.api_key = 'your-api-key'
 
 
-# Function to load dataset and check for embeddings
+# Function to generate embeddings for a given text.
+
+def get_embedding(text):
+
+    if not isinstance(text, str):
+
+        raise ValueError("Input must be a string")
+
+    
+
+    preprocessed_text = text  # Assumes API handles preprocessing, but could be more explicit.
+
+
+    try:
+
+        response = openai.Embedding.create(
+
+            input=preprocessed_text,
+
+            model="text-embedding-ada-002"
+
+        )
+
+        embedding = response['data'][0]['embedding']
+
+    except Exception as e:
+
+        raise Exception("Error generating embedding: " + str(e))
+
+
+    embedding_list = np.array(embedding).tolist()
+
+
+    return embedding_list
+
+
+# Function should include a more robust method for parsing embeddings, to avoid incorrect operations on malformed data.
 
 def load_dataset(input_path):
 
-    """Loads the dataset and checks if embeddings exist.
-
-    
-
-    Args:
-
-        input_path (str): Path to the dataset file.
-
-        
-
-    Returns:
-
-        DataFrame: The loaded dataset with or without existing embeddings.
-
-    """
-
-    # Load dataset and attempt to parse embeddings as lists if they exist
-
     df = pd.read_csv(input_path, converters={'embedding': literal_eval}, na_values=['None'])
 
-    
-
-    # Determine if embeddings column has valid entries
 
     embeddings_exist = not df['embedding'].isnull().all()
+
 
     return df, embeddings_exist
 
 
-# Function to generate embeddings for dataset if needed
+# This function can be enhanced using vectorized operations or parallel processing.
 
 def generate_embeddings(df):
-
-    """Generates embeddings for the dataset if they do not exist.
-
-    
-
-    Args:
-
-        df (DataFrame): The dataset DataFrame.
-
-        
-
-    Returns:
-
-        DataFrame: The dataset with generated embeddings.
-
-    """
-
-    # If embeddings do not exist, create them using the get_embedding function
 
     if 'embedding' not in df.columns or df['embedding'].isnull().all():
 
         df['embedding'] = df['content'].apply(lambda x: get_embedding(x) if isinstance(x, str) else np.nan)
 
+
     return df
 
 
-# Function to search through the dataset using embeddings
+# Optimization can be done using vectorized operations or scikit-learn utilities for similarity calculation.
 
 def search_dataset(df, query, top_n=3):
 
-    """Searches through the dataset using embedding similarity.
-
-    
-
-    Args:
-
-        df (DataFrame): The dataset with embeddings.
-
-        query (str): The search query to find similar content.
-
-        top_n (int): The number of top similar results to return.
-
-        
-
-    Returns:
-
-        DataFrame: The top similar results to the query.
-
-    """
-
-    # Convert query to embedding vector
-
     query_embedding = get_embedding(query)
 
-    
 
-    # Compute cosine similarity and sort results
+    # Instantiate a cosine_similarity function, vectorized operations can lead to significant speed up.
 
     df['similarity'] = df['embedding'].apply(lambda emb: cosine_similarity(emb, query_embedding))
 
+
     top_results = df.nlargest(top_n, 'similarity')
 
-    
 
     return top_results
 
-
-# Argument parser setup
 
 parser = argparse.ArgumentParser(description="Search PDF Embeddings")
 
@@ -128,22 +100,13 @@ parser.add_argument('file_path', help='Path to the dataset file.')
 parser.add_argument('query', help='Search query.')
 
 
-# Parse command-line arguments
-
 args = parser.parse_args()
 
 
-# Main program execution
-
 if __name__ == "__main__":
-
-    # Load the dataset and check for existing embeddings
 
     dataset, embeddings_exist = load_dataset(args.file_path)
 
-    
-
-    # Generate embeddings if they do not exist
 
     if not embeddings_exist:
 
@@ -151,22 +114,17 @@ if __name__ == "__main__":
 
         dataset = generate_embeddings(dataset)
 
-    
-
-    # Perform search on the dataset with the query
 
     results = search_dataset(dataset, args.query)
 
-    
-
-    # Display top search results
 
     for index, row in results.iterrows():
 
+        # Provide formatted output for better readability.
+
         print(f"ID: {row['id']}, Content: {row['content']}, Similarity: {row['similarity']}")
 
-        
 
-    # Future optimization: Implement batch processing for generating embeddings to minimize API calls.
+    # Future optimization could include batch embedding generation to minimize API usage.
 
-    # Future optimization: Use vectorized operations for similarity calculation instead of apply.
+    # Future optimization: pre-calculate and store similarity scores for faster search operations.
